20c9f7b77d3de1f4bae6b386d3afee6bcc8e0c2c
Johannes <github.finch886@passmail.net>
2025-09-01 23:13:55 +0200

    updated abstractcomponent => streamlined method declaration and derived overrides

--- src/Abstract/AbstractComponent.ts
+++ src/Abstract/AbstractComponent.ts
@@ -1,57 +1,71 @@
-import { Box } from "@ludeschersoftware/types";
+import { Box, Optional } from "@ludeschersoftware/types";
 import InputStateInterface from "../Interfaces/InputStateInterface";
 import ContentManager from "../Manager/ContentManager";
 
 abstract class AbstractComponent implements Box {
-    public width: number;
-    public height: number;
-
-    private m_x: number;
-    private m_y: number;
+    private m_box: Box;
     private m_parent_component: AbstractComponent | undefined;
     private m_child_components: AbstractComponent[];
 
-    constructor(box: Box) {
-        this.width = box.width;
-        this.height = box.height;
-        this.m_x = box.x;
-        this.m_y = box.y;
+    constructor(box?: Optional<Box>) {
+        this.m_box = Object.assign({
+            x: 0,
+            y: 0,
+            width: 0,
+            height: 0,
+        }, box);
         this.m_parent_component = undefined;
         this.m_child_components = [];
     }
 
     public get x(): number {
         if (this.m_parent_component === undefined) {
-            return this.m_x;
+            return this.m_box.x;
         }
 
-        return this.m_x + this.m_parent_component.x;
+        return this.m_box.x + this.m_parent_component.x;
     }
 
     public set x(x: number) {
-        this.m_x = x;
+        this.m_box.x = x;
     }
 
     public get y(): number {
         if (this.m_parent_component === undefined) {
-            return this.m_y;
+            return this.m_box.y;
         }
 
-        return this.m_y + this.m_parent_component.y;
+        return this.m_box.y + this.m_parent_component.y;
     }
 
     public set y(y: number) {
-        this.m_y = y;
+        this.m_box.y = y;
+    }
+
+    public get width(): number {
+        return this.m_box.width;
+    }
+
+    public set width(width: number) {
+        this.m_box.width = width;
+    }
+
+    public get height(): number {
+        return this.m_box.height;
+    }
+
+    public set height(height: number) {
+        this.m_box.height = height;
     }
 
     public SetParentComponent(parent: AbstractComponent): void {
         this.m_parent_component = parent;
     }
 
-    public abstract Initialize(): void;
-    public abstract LoadContent(contentManager: ContentManager): void;
-    public abstract Update(deltaTime: number, inputState: InputStateInterface): void | false;
-    public abstract Draw(context: CanvasRenderingContext2D, deltaTime: number): void | false;
+    public Initialize(): void { }
+    public LoadContent(_contentManager: ContentManager): void { }
+    public Update(_deltaTime: number, _inputState: InputStateInterface): void | false { }
+    public Draw(_context: CanvasRenderingContext2D, _deltaTime: number): void | false { }
 
     public GetComponents(): AbstractComponent[] {
         return this.m_child_components;
