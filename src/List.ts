934714279b4ff0827bad9314118cb3f708746342
Johannes <github.finch886@passmail.net>
2025-09-08 14:31:14 +0200

    updated list implementation

--- src/List.ts
+++ src/List.ts
@@ -1,234 +1,119 @@
+type ComparerType<T> = (a: T, b: T) => boolean;
+type ItemCallbackType<T> = (item: T) => boolean;
+
 class List<T> {
-    private m_index: number;
-    private m_data: Map<number, T>;
-    private m_lookup: Map<T, number>;
-    // private m_data_key_lookup: Map<number, number>;
+    private m_items: T[];
+    private m_comparer: ComparerType<T>;
 
-    public constructor(items?: T[]) {
-        this.m_index = 0;
-        this.m_data = new Map();
-        this.m_lookup = new Map();
-
-        if (items) {
-            for (const item of items) {
-                this.Add(item);
-            }
-        }
+    constructor(collection?: T[], comparer?: ComparerType<T>) {
+        this.m_items = collection ? [...collection] : [];
+        this.m_comparer = comparer ?? ((a, b) => a === b);
     }
 
     public get Count(): number {
-        return this.m_data.size;
+        return this.m_items.length;
     }
 
     public *Items(): Generator<T> {
-        for (const item of this.m_data.values()) {
-            yield item;
-        }
+        yield* this.m_items;
     }
 
-    public Add(item: T): typeof this {
-        this.m_data.set(this.m_index, item);
-        this.m_lookup.set(item, this.m_index++);
+    public Add(item: T): this {
+        this.m_items.push(item);
 
         return this;
     }
 
-    public AddRange(...collection: T[]): typeof this {
-        for (const item of collection) {
-            this.Add(item);
-        }
+    public AddRange(collection: T[]): this {
+        this.m_items.push(...collection);
 
         return this;
     }
 
-    public Insert(index: number, item: T): typeof this {
-        const EXISTING: T | undefined = this.m_data.get(index);
-
-        if (EXISTING !== undefined) {
-            this.m_lookup.delete(EXISTING);
+    public Insert(index: number, item: T): this {
+        if ((index < 0) || (index > this.m_items.length)) {
+            throw new RangeError("Index out of range.");
         }
 
-        this.m_data.set(index, item);
-        this.m_lookup.set(item, index);
-
-        if (index >= this.m_index) {
-            this.m_index = index + 1;
-        }
+        this.m_items.splice(index, 0, item);
 
         return this;
     }
 
     public Remove(item: T): boolean {
-        const INDEX: number | undefined = this.m_lookup.get(item);
+        const INDEX: number = this.IndexOf(item);
 
-        if (INDEX === undefined) {
-            return false;
-        }
+        if (INDEX >= 0) {
+            this.m_items.splice(INDEX, 1);
 
-        this.m_lookup.delete(item);
-
-        return this.m_data.delete(INDEX);
-    }
-
-    public RemoveAt(index: number): boolean {
-        const ITEM: T | undefined = this.m_data.get(index);
-
-        if (ITEM !== undefined) {
-            this.m_lookup.delete(ITEM);
-        }
-
-        return this.m_data.delete(index);
-    }
-
-    public Clear(): typeof this {
-        this.m_data.clear();
-        this.m_lookup.clear();
-        this.m_index = 0;
-
-        return this;
-    }
-
-    public Contains(item: T): boolean {
-        return this.m_lookup.has(item);
-    }
-
-    public IndexOf(item: T): number {
-        return this.m_lookup.get(item) ?? -1;
-    }
-
-    public Sort(callback?: (a: T, b: T) => number): typeof this {
-        const TEMP = this.ToArray().sort(callback);
-
-        this.Clear();
-
-        for (const item of TEMP) {
-            this.Add(item);
-        }
-
-        return this;
-    }
-
-    public Reverse(): typeof this {
-        const TEMP = this.ToArray().reverse();
-
-        this.Clear();
-
-        for (const item of TEMP) {
-            this.Add(item);
-        }
-
-        return this;
-    }
-
-    public ToArray(): T[] {
-        return Array.from(this.m_data.values());
-    }
-
-    public Find(callback: (item: T) => boolean): T | undefined {
-        for (const item of this.m_data.values()) {
-            if (callback(item)) {
-                return item;
-            }
-        }
-
-        return undefined;
-    }
-
-    public Filter(callback: (item: T) => boolean): List<T> {
-        const RESULT: List<T> = new List<T>();
-
-        for (const item of this.m_data.values()) {
-            if (callback(item)) {
-                RESULT.Add(item);
-            }
-        }
-
-        return RESULT;
-    }
-
-    public Map<U>(callback: (item: T) => U): List<U> {
-        const RESULT: List<U> = new List<U>();
-
-        for (const item of this.m_data.values()) {
-            RESULT.Add(callback(item));
-        }
-
-        return RESULT;
-    }
-
-    public Clone(): List<T> {
-        return new List<T>(this.ToArray());
-    }
-
-    public First(): T | undefined {
-        for (const item of this.m_data.values()) {
-            return item;
-        }
-
-        return undefined;
-    }
-
-    public Last(): T | undefined {
-        const VALUES: T[] = Array.from(this.m_data.values());
-
-        return VALUES.length > 0 ? VALUES[VALUES.length - 1] : undefined;
-    }
-
-    public Any(callback?: (item: T) => boolean): boolean {
-        if (!callback) {
-            return this.Count > 0;
-        }
-
-        for (const item of this.m_data.values()) {
-            if (callback(item)) {
-                return true;
-            }
+            return true;
         }
 
         return false;
     }
 
-    public All(callback: (item: T) => boolean): boolean {
-        for (const item of this.m_data.values()) {
-            if (!callback(item)) {
-                return false;
-            }
+    public RemoveAt(index: number): this {
+        if ((index < 0) || (index >= this.m_items.length)) {
+            throw new RangeError("Index out of range.");
         }
 
-        return true;
-    }
-
-    public Distinct(): List<T> {
-        const SEEN = new Set<T>();
-        const RESULT = new List<T>();
-
-        for (const item of this.m_data.values()) {
-            if (!SEEN.has(item)) {
-                SEEN.add(item);
-                RESULT.Add(item);
-            }
-        }
-
-        return RESULT;
-    }
-
-    public ToJSON(): string {
-        return JSON.stringify(this.ToArray());
-    }
-
-    public static FromJSON<U>(json: string, parser?: (raw: any) => U): List<U> {
-        const RAW: any = JSON.parse(json);
-        const ITEMS: U[] = parser ? RAW.map(parser) : RAW;
-
-        return new List<U>(ITEMS);
-    }
-
-    public ForEach(callback: (item: T, index: number) => void): typeof this {
-        for (const [index, item] of this.m_data.entries()) {
-            callback(item, index);
-        }
+        this.m_items.splice(index, 1);
 
         return this;
+    }
+
+    public Contains(item: T): boolean {
+        return this.m_items.some(x => this.m_comparer(x, item));
+    }
+
+    public IndexOf(item: T): number {
+        return this.m_items.findIndex(x => this.m_comparer(x, item));
+    }
+
+    public Find(callback: ItemCallbackType<T>): T | undefined {
+        return this.m_items.find(callback);
+    }
+
+    public FindIndex(callback: ItemCallbackType<T>): number {
+        return this.m_items.findIndex(callback);
+    }
+
+    public Clear(): this {
+        this.m_items = [];
+
+        return this;
+    }
+
+    public Sort(comparer?: (a: T, b: T) => number): this {
+        this.m_items.sort(comparer);
+
+        return this;
+    }
+
+    public ToArray(): T[] {
+        return [...this.m_items];
+    }
+
+    public Get(index: number): T {
+        if ((index < 0) || (index >= this.m_items.length)) {
+            throw new RangeError("Index out of range.");
+        }
+
+        return this.m_items[index];
+    }
+
+    public Set(index: number, value: T): this {
+        if ((index < 0) || (index >= this.m_items.length)) {
+            throw new RangeError("Index out of range.");
+        }
+
+        this.m_items[index] = value;
+
+        return this;
+    }
+
+    public Clone(): List<T> {
+        return new List([...this.m_items], this.m_comparer);
     }
 
     public IsEmpty(): boolean {
@@ -237,6 +122,52 @@ class List<T> {
 
     public [Symbol.iterator](): Iterator<T> {
         return this.Items();
+    }
+
+    public Where(callback: ItemCallbackType<T>): List<T> {
+        return new List(this.m_items.filter(callback), this.m_comparer);
+    }
+
+    public Select<U>(selector: (item: T) => U): List<U> {
+        return new List(this.m_items.map(selector));
+    }
+
+    public Any(callback?: ItemCallbackType<T>): boolean {
+        return callback ? this.m_items.some(callback) : this.m_items.length > 0;
+    }
+
+    public All(callback: ItemCallbackType<T>): boolean {
+        return this.m_items.every(callback);
+    }
+
+    public First(callback?: ItemCallbackType<T>): T | undefined {
+        return callback ? this.m_items.find(callback) : this.m_items[0];
+    }
+
+    public FirstOrDefault(callback?: ItemCallbackType<T>, defaultValue?: T): T | undefined {
+        const FOUND: T | undefined = callback ? this.m_items.find(callback) : this.m_items[0];
+
+        return FOUND === undefined ? defaultValue : FOUND;
+    }
+
+    public Last(callback?: ItemCallbackType<T>): T | undefined {
+        if (callback) {
+            for (let i = this.m_items.length - 1; i >= 0; i--) {
+                if (callback(this.m_items[i])) {
+                    return this.m_items[i];
+                }
+            }
+
+            return undefined;
+        }
+
+        return this.m_items[this.m_items.length - 1];
+    }
+
+    public LastOrDefault(callback?: ItemCallbackType<T>, defaultValue?: T): T | undefined {
+        const FOUND: T | undefined = this.Last(callback);
+
+        return FOUND === undefined ? defaultValue : FOUND;
     }
 }
 
